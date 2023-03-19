66011b249609b2da3c78a3f3afda33ea50d92d4f
yunchang <yunchang@mit.edu>
2023-03-18 22:43:31 -0400

    move common vlc utils from kimera-distributed (#8)

--- include/kimera_multi_lcd/utils.h
+++ include/kimera_multi_lcd/utils.h
@@ -7,12 +7,31 @@
 #pragma once
 
 #include <DBoW2/DBoW2.h>
+#include <pose_graph_tools/BowQuery.h>
 #include <pose_graph_tools/BowVector.h>
+#include <pose_graph_tools/PoseGraph.h>
+#include <pose_graph_tools/PoseGraphEdge.h>
+#include <pose_graph_tools/VLCFrameMsg.h>
+#include <pose_graph_tools/VLCFrameQuery.h>
+
+#include "kimera_multi_lcd/types.h"
 
 namespace kimera_multi_lcd {
 void BowVectorToMsg(const DBoW2::BowVector& bow_vec, pose_graph_tools::BowVector* msg);
 
 void BowVectorFromMsg(const pose_graph_tools::BowVector& msg,
                       DBoW2::BowVector* bow_vec);
+
+void VLCFrameToMsg(const VLCFrame& frame, pose_graph_tools::VLCFrameMsg* msg);
+void VLCFrameFromMsg(const pose_graph_tools::VLCFrameMsg& msg, VLCFrame* frame);
+
+void VLCEdgeToMsg(const VLCEdge& edge, pose_graph_tools::PoseGraphEdge* msg);
+void VLCEdgeFromMsg(const pose_graph_tools::PoseGraphEdge& msg, VLCEdge* edge);
+
+// Compute the payload size in a BowQuery message
+size_t computeBowQueryPayloadBytes(const pose_graph_tools::BowQuery& msg);
+
+// Compute the payload size of a VLC frame
+size_t computeVLCFramePayloadBytes(const pose_graph_tools::VLCFrameMsg& msg);
 
 }  // namespace kimera_multi_lcd
--- src/utils.cpp
+++ src/utils.cpp
@@ -4,9 +4,7 @@
  * Authors: Yulun Tian (yulun@mit.edu)
  */
 
-#include <kimera_multi_lcd/utils.h>
-#include <ros/console.h>
-
+#include "kimera_multi_lcd/utils.h"
 #include <fstream>
 
 namespace kimera_multi_lcd {
@@ -29,4 +27,71 @@ void BowVectorFromMsg(const pose_graph_tools::BowVector& msg,
   }
 }
 
+void VLCFrameToMsg(const VLCFrame& frame, pose_graph_tools::VLCFrameMsg* msg) {
+  frame.toROSMessage(msg);
+}
+
+void VLCFrameFromMsg(const pose_graph_tools::VLCFrameMsg& msg, VLCFrame* frame) {
+  *frame = VLCFrame(msg);
+}
+
+void VLCEdgeToMsg(const VLCEdge& edge, pose_graph_tools::PoseGraphEdge* msg) {
+  // Yulun: this function currently does not assign covariance!
+
+  msg->robot_from = edge.vertex_src_.first;
+  msg->key_from = edge.vertex_src_.second;
+  msg->robot_to = edge.vertex_dst_.first;
+  msg->key_to = edge.vertex_dst_.second;
+  msg->type = pose_graph_tools::PoseGraphEdge::LOOPCLOSE;
+
+  gtsam::Pose3 pose = edge.T_src_dst_;
+  gtsam::Quaternion quat = pose.rotation().toQuaternion();
+  gtsam::Point3 position = pose.translation();
+
+  msg->pose.orientation.x = quat.x();
+  msg->pose.orientation.y = quat.y();
+  msg->pose.orientation.z = quat.z();
+  msg->pose.orientation.w = quat.w();
+
+  msg->pose.position.x = position.x();
+  msg->pose.position.y = position.y();
+  msg->pose.position.z = position.z();
+}
+
+void VLCEdgeFromMsg(const pose_graph_tools::PoseGraphEdge& msg, VLCEdge* edge) {
+  edge->vertex_src_ = std::make_pair(msg.robot_from, msg.key_from);
+  edge->vertex_dst_ = std::make_pair(msg.robot_to, msg.key_to);
+
+  gtsam::Rot3 rotation(msg.pose.orientation.w,
+                       msg.pose.orientation.x,
+                       msg.pose.orientation.y,
+                       msg.pose.orientation.z);
+
+  gtsam::Point3 position(msg.pose.position.x, msg.pose.position.y, msg.pose.position.z);
+
+  gtsam::Pose3 T_src_dst(rotation, position);
+  edge->T_src_dst_ = T_src_dst;
+}
+
+size_t computeBowQueryPayloadBytes(const pose_graph_tools::BowQuery& msg) {
+  size_t bytes = 0;
+  bytes += sizeof(msg.robot_id);
+  bytes += sizeof(msg.pose_id);
+  bytes += sizeof(msg.bow_vector.word_ids[0]) * msg.bow_vector.word_ids.size();
+  bytes += sizeof(msg.bow_vector.word_values[0]) * msg.bow_vector.word_values.size();
+  return bytes;
+}
+
+size_t computeVLCFramePayloadBytes(const pose_graph_tools::VLCFrameMsg& msg) {
+  size_t bytes = 0;
+  bytes += sizeof(msg.robot_id);
+  bytes += sizeof(msg.pose_id);
+  // descriptors
+  bytes += sizeof(msg.descriptors_mat);
+  bytes += sizeof(msg.descriptors_mat.data[0]) * msg.descriptors_mat.data.size();
+  // keypoints
+  bytes += sizeof(msg.versors);
+  bytes += sizeof(msg.versors.data[0]) * msg.versors.data.size();
+  return bytes;
+}
 }  // namespace kimera_multi_lcd
