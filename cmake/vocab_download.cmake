e9f73da9213b72c370dc079a86db354e7c0654bb
Hyungtae Lim <35317311+LimHyungTae@users.noreply.github.com>
2024-08-24 21:48:41 -0400

    Solve failure in the automatic download in #3 (#4)
    * Solve failure in the automatic download in #3
    
    * Transfer repository and change directory to MIT-SPARK
    
    * (minor) Fix typo

--- cmake/vocab_download.cmake
+++ cmake/vocab_download.cmake
@@ -1,21 +1,22 @@
-### Download and unzip the vocabularly file
+### Download and unzip the vocabulary file
+message("[Kimera-Multi-LCD] Trying to download and unzip the vocabulary file...")
 if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/vocab/mit_voc.yml)
-  message(STATUS "Downloading vocabulary file from drive.")
+  message("[Kimera-Multi-LCD] Downloading vocabulary file from drive.")
   file(DOWNLOAD
-       https://drive.google.com/uc?export=download&confirm=9iBg&id=1N4y0HbgA3PHQ73ZxFJvy5dgvV_0cTBYF
+       https://github.com/MIT-SPARK/kimera-multi-vocab/raw/master/vocab.zip
        ${CMAKE_CURRENT_SOURCE_DIR}/vocab.zip
-       SHOW_PROGRESS
        STATUS voc_download_success
-       TIMEOUT 60)
+       )
   if(voc_download_success)
-    message(STATUS "Unzipping vocabulary file.")
+    message("[Kimera-Multi-LCD] Unzipping vocabulary file...")
 
     execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${CMAKE_CURRENT_SOURCE_DIR}/vocab.zip
                             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/)
-    execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${CMAKE_CURRENT_SOURCE_DIR}/vocab.zip)
-  else(voc_download_success)
-    message(STATUS "Failed to download vocabulary file. Please download manually.")
-  endif(voc_download_success)
+    execute_process(COMMAND ${CMAKE_COMMAND} -E rm ${CMAKE_CURRENT_SOURCE_DIR}/vocab.zip)
+    message("[Kimera-Multi-LCD] Complete! Check your `${Kimera-Multi-LCD}/vocab/mit_voc.yml`.")
+  else()
+    message(FATAL_ERROR "[Kimera-Multi-LCD] Failed to download vocabulary file. Please download manually.")
+  endif()
 else()
-  message(STATUS "Vocabulary file exists, will not download.")
+  message("[Kimera-Multi-LCD] Vocabulary file exists, will not download.")
 endif()
